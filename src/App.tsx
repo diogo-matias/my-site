41c11151860bc67ebf67dfa555fda0887a5dd9ca
Diogo Macedo <diogomaccedo200@gmail.com>
2023-08-20 13:16:06 -0300

    refactor: remove log

--- src/App.tsx
+++ src/App.tsx
@@ -56,8 +56,6 @@ function App() {
                     }
                 );
             } else {
-                console.log("Error to get localization");
-
                 return resolve({ latitude: undefined, longitude: undefined });
             }
         });
